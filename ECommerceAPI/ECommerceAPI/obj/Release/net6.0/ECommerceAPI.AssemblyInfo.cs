bea118555925f1d6baad917a85770cfb7fe2f827
Alpay Kuzu <147747941+alpaykuzu@users.noreply.github.com>
2024-07-16 11:51:11 +0300

    Update
    Onion architecture dizaynı uygulandı

--- ECommerceAPI/ECommerceAPI/obj/Release/net6.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/ECommerceAPI/obj/Release/net6.0/ECommerceAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef5ff7ccb94037d0985dcc3f6efa42c9c91f187")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
