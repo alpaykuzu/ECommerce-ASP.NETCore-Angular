09b578d76e07a53cacc3c41e6ef402fadd53846d
Alpay Kuzu <147747941+alpaykuzu@users.noreply.github.com>
2024-07-17 17:11:54 +0300

    Update

---
+++ ECommerceClient/src/app/admin/components/customers/customers.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersComponent } from './customers.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    CustomersComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: "", component:CustomersComponent}
+    ])
+  ]
+})
+export class CustomersModule { }
--- ECommerceClient/src/app/app.module.ts
+++ ECommerceClient/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    UiModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
