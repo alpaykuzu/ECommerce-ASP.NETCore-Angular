4440a0e3b3cc5ee4203ba1e13ec951c8c138d023
Alpay Kuzu <147747941+alpaykuzu@users.noreply.github.com>
2024-07-18 15:40:14 +0300

    Update

--- ECommerceAPI/Presentation/ECommerceAPI.API/Program.cs
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistenceServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => 
+    policy.WithOrigins("http://localhost:4200/", "https://localhost:4200/").AllowAnyHeader().AllowAnyMethod()
+));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
